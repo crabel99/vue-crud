5961de5f368b23aa3070b3300c988b29416d1ccb
Cal Abel <crabel@Cals-MacBook-Pro.local>
2020-04-21 09:37:55 -0400

    ES locale need crm: login, profile, and routes

--- examples/crm/locales/index.js
+++ examples/crm/locales/index.js
@@ -4,12 +4,14 @@ import store from '@/store/index'
 
 import pl from './pl/index'
 import en from './en/index'
+import es from './es/index'
 
 Vue.use(VueI18n)
 
 const messages = {
   pl: pl,
   en: en,
+  es: es,
 }
 
 export default new VueI18n({
--- examples/crm/locales/vuetify.js
+++ examples/crm/locales/vuetify.js
@@ -1,5 +1,7 @@
 import pl from 'vuetify/src/locale/pl.ts'
+import es from 'vuetify/src/locale/es.ts'
 
 export default {
   pl,
+  es,
 }
